376d4dd9bed768f3f3f7a641cb5a157188e516d8
RandomGamingDev <randomgamingdev@gmail.com>
2024-02-03 12:36:42 -0500

    changed unit test a bit

--- example/example.js
+++ example/example.js
@@ -2,9 +2,9 @@ const test = new UnitTester("Root Test", "The test that starts it all! (it also 
   () => {
   },
   [
-    new UnitTester("This subtest is mean to fail!", "Something nice about this unit tester is that it keeps on testing even after something fails, whcih means that you'll be able to check everything else in case you're having trouble with a specific problem which especially helps if you can find other issues happening at the same time since the issues could be causing one another or correlated! (Note: Although tests following will still run, tests that are children of this test won't)",
+    new UnitTester("This subtest is mean to fail if uncommented!", "Something nice about this unit tester is that it keeps on testing even after something fails, whcih means that you'll be able to check everything else in case you're having trouble with a specific problem which especially helps if you can find other issues happening at the same time since the issues could be causing one another or correlated! (Note: Although tests following will still run, tests that are children of this test won't)",
       () => {
-        UnitTester.assert(false);
+        //UnitTester.assert(false);
       }
     ),
     new UnitTester("UnitTester.assert(test: bool, error_msg: String)", "This test asserts whether or not its input is true",
@@ -29,8 +29,12 @@ const test = new UnitTester("Root Test", "The test that starts it all! (it also 
 console.log("RESULTS: ");
 
 // This logs any failed tests belonging to the root test (if the root test fails you'll want to check the root test's state)
-console.log("Failed Tests (if the line below this is empty there are no failures): ");
-console.log(test.failed_sub_tests);
+if (test.failed_sub_tests.length == 0)
+  console.log("The unit test executed successfully");
+else {
+  console.log("Failed Tests: ");
+  console.log(test.failed_sub_tests);
+}
 
 // This logs everything in the test, not just errors!
 console.log("All Test Results: ");
